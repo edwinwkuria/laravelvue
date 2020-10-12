dc8d12d66e757b9dfa145d4cdfddb4884e5a671d
EdwinWKuria <edwin.w.kuria@gmail.com>
2020-10-12 16:18:03 +0300

    Vue component

---
+++ resources/js/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+<ul class="list-group">
+  <li class="list-group-item" v-text="message"></li>
+</ul>
+</template>
+<script>
+export default {
+    data: function(){
+        return{
+            listItems: ["Got to the shop", "Go to the Market", "Clean the house"],
+            message : "Vue JS"
+        }
+    },
+    mounted() {
+            console.log('Component mounted.')
+        }
+}
+</script>
